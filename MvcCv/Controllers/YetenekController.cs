493c7418aca50a033007032839286999e66cda90
hkurnali <huseyinkurnali@hotmail.com>
2023-05-24 00:28:03 +0300

    Güncellendi

--- MvcCv/Controllers/YetenekController.cs
+++ MvcCv/Controllers/YetenekController.cs
@@ -16,5 +16,40 @@ namespace MvcCv.Controllers
             var values = repo.List();
             return View(values);
         }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+        return View();
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var values = repo.TGet(id);
+
+            repo.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekGuncelle(int id)
+        {
+            var values = repo.TGet(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult YetenekGuncelle(TblYeteneklerim p)
+        {
+            var x = repo.TGet(p.ID);
+            x.Yetenek = p.Yetenek;
+            x.Oran = p.Oran;
+           
+            repo.TUpdate(x);
+            return RedirectToAction("Index");
+        }
     }
 }
